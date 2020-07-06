2ad624d523864a6286f2fb255af2a0870712ea00
Nick Schmidt <nick.schmidt3@gmail.com>
2020-07-05 16:25:29 -0800

    re-organized so message tabe was with all of the other pre declarations

--- nxos/nxapi-get-routes-json.py
+++ nxos/nxapi-get-routes-json.py
@@ -75,6 +75,74 @@ payload={
   }
 }
 
+# Set HTTP Error + Verbosity table. Due to the use of max(min()), verbosity count becomes a numerical range that caps off and prevents array issues
+# Credit where due - https://gist.github.com/bl4de/3086cf26081110383631 by bl4de
+httperrors = {
+  100: ('Continue', 'Request received, please continue'),
+  101: ('Switching Protocols',
+        'Switching to new protocol; obey Upgrade header'),
+
+  200: ('OK', 'Request fulfilled, document follows'),
+  201: ('Created', 'Document created, URL follows'),
+  202: ('Accepted',
+        'Request accepted, processing continues off-line'),
+  203: ('Non-Authoritative Information', 'Request fulfilled from cache'),
+  204: ('No Content', 'Request fulfilled, nothing follows'),
+  205: ('Reset Content', 'Clear input form for further input.'),
+  206: ('Partial Content', 'Partial content follows.'),
+
+  300: ('Multiple Choices',
+        'Object has several resources -- see URI list'),
+  301: ('Moved Permanently', 'Object moved permanently -- see URI list'),
+  302: ('Found', 'Object moved temporarily -- see URI list'),
+  303: ('See Other', 'Object moved -- see Method and URL list'),
+  304: ('Not Modified',
+        'Document has not changed since given time'),
+  305: ('Use Proxy',
+        'You must use proxy specified in Location to access this '
+        'resource.'),
+  307: ('Temporary Redirect',
+        'Object moved temporarily -- see URI list'),
+
+  400: ('Bad Request',
+        'Bad request syntax or unsupported method'),
+  401: ('Unauthorized',
+        'No permission -- see authorization schemes'),
+  402: ('Payment Required',
+        'No payment -- see charging schemes'),
+  403: ('Forbidden',
+        'Request forbidden -- authorization will not help'),
+  404: ('Not Found', 'Nothing matches the given URI'),
+  405: ('Method Not Allowed',
+        'Specified method is invalid for this server.'),
+  406: ('Not Acceptable', 'URI not available in preferred format.'),
+  407: ('Proxy Authentication Required', 'You must authenticate with '
+        'this proxy before proceeding.'),
+  408: ('Request Timeout', 'Request timed out; try again later.'),
+  409: ('Conflict', 'Request conflict.'),
+  410: ('Gone',
+        'URI no longer exists and has been permanently removed.'),
+  411: ('Length Required', 'Client must specify Content-Length.'),
+  412: ('Precondition Failed', 'Precondition in headers is false.'),
+  413: ('Request Entity Too Large', 'Entity is too large.'),
+  414: ('Request-URI Too Long', 'URI is too long.'),
+  415: ('Unsupported Media Type', 'Entity body in unsupported format.'),
+  416: ('Requested Range Not Satisfiable',
+        'Cannot satisfy request range.'),
+  417: ('Expectation Failed',
+        'Expect condition could not be satisfied.'),
+
+  500: ('Internal Server Error', 'Server got itself in trouble'),
+  501: ('Not Implemented',
+        'Server does not support this operation'),
+  502: ('Bad Gateway', 'Invalid responses from another server/proxy.'),
+  503: ('Service Unavailable',
+        'The server cannot process the request due to a high load'),
+  504: ('Gateway Timeout',
+        'The gateway server did not receive a timely response'),
+  505: ('HTTP Version Not Supported', 'Cannot fulfill request.'),
+}
+
 # Perform NX-API Processing - conditional based on certificate authentication
 try: 
   if client_cert_auth is False:
@@ -84,74 +152,9 @@ try:
   #response_json = response
   response.raise_for_status()
 except:
-  # Credit where due - https://gist.github.com/bl4de/3086cf26081110383631 by bl4de
-  httperrors = {
-    100: ('Continue', 'Request received, please continue'),
-    101: ('Switching Protocols',
-          'Switching to new protocol; obey Upgrade header'),
 
-    200: ('OK', 'Request fulfilled, document follows'),
-    201: ('Created', 'Document created, URL follows'),
-    202: ('Accepted',
-          'Request accepted, processing continues off-line'),
-    203: ('Non-Authoritative Information', 'Request fulfilled from cache'),
-    204: ('No Content', 'Request fulfilled, nothing follows'),
-    205: ('Reset Content', 'Clear input form for further input.'),
-    206: ('Partial Content', 'Partial content follows.'),
-
-    300: ('Multiple Choices',
-          'Object has several resources -- see URI list'),
-    301: ('Moved Permanently', 'Object moved permanently -- see URI list'),
-    302: ('Found', 'Object moved temporarily -- see URI list'),
-    303: ('See Other', 'Object moved -- see Method and URL list'),
-    304: ('Not Modified',
-          'Document has not changed since given time'),
-    305: ('Use Proxy',
-          'You must use proxy specified in Location to access this '
-          'resource.'),
-    307: ('Temporary Redirect',
-          'Object moved temporarily -- see URI list'),
-
-    400: ('Bad Request',
-          'Bad request syntax or unsupported method'),
-    401: ('Unauthorized',
-          'No permission -- see authorization schemes'),
-    402: ('Payment Required',
-          'No payment -- see charging schemes'),
-    403: ('Forbidden',
-          'Request forbidden -- authorization will not help'),
-    404: ('Not Found', 'Nothing matches the given URI'),
-    405: ('Method Not Allowed',
-          'Specified method is invalid for this server.'),
-    406: ('Not Acceptable', 'URI not available in preferred format.'),
-    407: ('Proxy Authentication Required', 'You must authenticate with '
-          'this proxy before proceeding.'),
-    408: ('Request Timeout', 'Request timed out; try again later.'),
-    409: ('Conflict', 'Request conflict.'),
-    410: ('Gone',
-          'URI no longer exists and has been permanently removed.'),
-    411: ('Length Required', 'Client must specify Content-Length.'),
-    412: ('Precondition Failed', 'Precondition in headers is false.'),
-    413: ('Request Entity Too Large', 'Entity is too large.'),
-    414: ('Request-URI Too Long', 'URI is too long.'),
-    415: ('Unsupported Media Type', 'Entity body in unsupported format.'),
-    416: ('Requested Range Not Satisfiable',
-          'Cannot satisfy request range.'),
-    417: ('Expectation Failed',
-          'Expect condition could not be satisfied.'),
-
-    500: ('Internal Server Error', 'Server got itself in trouble'),
-    501: ('Not Implemented',
-          'Server does not support this operation'),
-    502: ('Bad Gateway', 'Invalid responses from another server/proxy.'),
-    503: ('Service Unavailable',
-          'The server cannot process the request due to a high load'),
-    504: ('Gateway Timeout',
-          'The gateway server did not receive a timely response'),
-    505: ('HTTP Version Not Supported', 'Cannot fulfill request.'),
-  }
   if httperrors.get(response.status_code):
-    print ('HTTP Status Error ' + str(response.status_code) + ' ' + httperrors.get(response.status_code)[min(args.verbosity,1)])
+    print ('HTTP Status Error ' + str(response.status_code) + ' ' + httperrors.get(response.status_code)[max(min(args.verbosity,1),0)])
   else:
     print ('Unhandled HTTP Error ' + str(response.status_code) + '!' )
 # Do things with what was received by the API!
